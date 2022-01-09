239496ca8d084e9b2c9860aab17f3ffdadd9702f
JohnPena0115 <calmwinds06@gmail.com>
2022-01-09 09:54:57 -0500

    "Uploaded my attempt at Mortgage Calculator while following along with Mosh's code."

--- src/com/careerdevs/Intro/mortgagecalc/MortgageCalcMain.java
+++ src/com/careerdevs/Intro/mortgagecalc/MortgageCalcMain.java
@@ -1,8 +1,66 @@
 package com.careerdevs.Intro.mortgagecalc;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class MortgageCalcMain {
 
     public static void main(String[] args) {
+
+        /*
+
+        //My Initial Version
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = Float.parseFloat(scanner.nextLine());
+
+        System.out.print("Period(Years): ");
+        int loanDuration = Integer.parseInt(scanner.nextLine());
+
+        float monthlyInterest = annualInterest/1200;
+        int totalPayments = loanDuration * 12;
+
+        float monthlyInterestCompounded = (float)Math.pow(monthlyInterest+1, totalPayments);
+
+
+        float monthlyPayment = principal *  ( (monthlyInterest * monthlyInterestCompounded) / (monthlyInterestCompounded- 1) );
+
+        String monthlyPaymentFormatted = NumberFormat.getCurrencyInstance().format(monthlyPayment);
+
+        System.out.print("Mortgage: " + monthlyPaymentFormatted);
+
+
+         */
+
+        //Mosh Version
+
+        //Wants us to avoid using numeric literals in code. j
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                    * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                    / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
 
     }
 }
